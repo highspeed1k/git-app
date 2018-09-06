b722b1a92239fd8aaa302c1c424aae3dbbf97b89
Highspeed1k <spencerpaulbrown@gmail.com>
2018-09-06 14:25:53 -0500

    check completed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-class App extends Component {
+class App extends Component
+constructor(props) {
+    super(props);
+    this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+
+  }
+
+
   render() {
     return (
       <div className="App">
 
       <ul>
-       <ToDo />
-       <ToDo />
-      </ul> 
+      { this.state.todos.map( (todo, index) =>
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            )}
+      </ul>
 
     </div>
     );
